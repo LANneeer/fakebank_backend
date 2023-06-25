f833f352ab79a04192ff4b1f5ee8c1da29508a29
Temirlan Ilik <0penguin@mail.ru>
2023-06-25 12:30:15 +0500

    prepare to deploy

--- bank/settings.py
+++ bank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "ec2-13-51-56-169.eu-north-1.compute.amazonaws.com", "13.51.56.169"]
 
 
 # Application definition
